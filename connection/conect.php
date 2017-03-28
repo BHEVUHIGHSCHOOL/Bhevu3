2b8db9e5952dab17efd357150b8af0c3a62ff703
Terrost <21511349@dut4life.ac.za>
2017-03-28 13:56:04 +0200

    C# Solution Project Start

---
+++ connection/conect.php
@@ -0,0 +1,25 @@
+<?php 
+	ob_start();
+	$con = mysqli_connect("www.zanzosamazingdish.co.za","yxhhnsph","*3*NRdNXKb9jRRK","yxhhnsph_ds3(bhevu)");
+
+
+
+?>
+
+<?php
+
+if(isset($con))
+
+{
+
+}
+
+else
+
+{
+
+	die ('Could not connect'.mysqli_error());
+
+}
+
+?>
