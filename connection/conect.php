00f3321944d379fe35241aec3662bca65a7502bc
Mthokozisi20 <21511349@dut4life.ac.za>
2017-03-30 13:27:21 +0200

    Update connection

--- connection/conect.php
+++ connection/conect.php
@@ -1,44 +1,6 @@
 <?php 
 	ob_start();
 
-	$con = mysqli_connect("www.zanzosamazingdish.co.za","yxhhnsph","*3*NRdNXKb9jRRK","yxhhnsph_ds3(bhevu)");
-if(!$con)
-	{
-		die ("Could not connect:".mysql_error());
-	}
-	if(mysql_query("create database ds3(bhevu)",$con))
-	{
-		echo "Database creted </br>";
-	}
-	else
-	{
-		echo "Error creating database".mysql_error();
-	}
-	
-	mysql_select_db("ds3(bhevu)",$con);
-
-
 	$con = mysqli_connect("localhost","root","","ds3(bhevu)");
-
-
-
-
 ?>
 
-<?php
-
-if(isset($con))
-
-{
-
-}
-
-else
-
-{
-
-	die ('Could not connect'.mysqli_error());
-
-}
-
-?>
